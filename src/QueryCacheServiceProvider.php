c89659ee516448f13099c13dfbe8d1a7daf478ef
ratul <ratul38@gmail.com>
2024-12-19 14:37:32 +0600

    optimized code for memory

--- src/QueryCacheServiceProvider.php
+++ src/QueryCacheServiceProvider.php
@@ -4,35 +4,16 @@ namespace Hishabee\LaravelQueryCache;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\Cache;
 
 class QueryCacheServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom(
-            __DIR__.'/../config/query-cache.php', 'query-cache'
-        );
-
-        $this->app->singleton(QueryCacheService::class, function ($app) {
-            return new QueryCacheService();
-        });
-
-        // Register the cache macro
         Builder::macro('cache', function ($duration = null) {
             $this->shouldCache = true;
             $this->cacheDuration = $duration;
             return $this;
         });
-    }
-
-    public function boot()
-    {
-        // Publish config
-        $this->publishes([
-            __DIR__.'/../config/query-cache.php' => config_path('query-cache.php'),
-        ], 'query-cache-config');
-
-        // Initialize caching service
-        $this->app->make(QueryCacheService::class)->enableQueryCache();
     }
 }
